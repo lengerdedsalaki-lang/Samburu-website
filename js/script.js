053f05c90189386405a8d08a876fac66a45f467d
lengerdedsalaki-lang <lengerded.salaki@student.moringaschool.com>
2025-10-17 11:20:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// document object model for interactivity
+// click explore samburu should show a mesg
+document.getElementById('explorebtn').addEventListener('click', function() {
+    alert('Welcome, page under development.');
+});
